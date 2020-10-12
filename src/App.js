b49dccbaf8af7908dc05c950562c7a9b9afa96e9
Nikhil <41849216+Nikgithubit@users.noreply.github.com>
2020-10-13 01:54:41 +0530

    setting up folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppNavbar from './components/AppNavbar';
 
 function App() {
   return (
     <div className="App">
+      <AppNavbar />
      <p>Hi! This is main app component</p>
     </div>
   );
