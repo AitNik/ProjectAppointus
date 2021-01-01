4d6bbd1606dd7e7cd9cd6bd742a78626154f5586
Nikhil <41849216+Nikgithubit@users.noreply.github.com>
2021-01-01 16:47:19 +0530

    ignore

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import brandIcon from "../assets/Images/brandIcon.png"
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { connect } from 'react-redux'
 
 function AppNavbar(props) {
@@ -33,19 +33,12 @@ function AppNavbar(props) {
               ) : null
             }
             <Nav.Link href='/aboutus' className="mr-2 ml-2">About us</Nav.Link>
-            <NavDropdown title="User Name"  variant="dark" bg="dark">
-              <NavDropdown.Item href="#action/3.1">Action 1</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Action 2</NavDropdown.Item>
-            </NavDropdown>            
-            
           </Nav>
           {
             props.logged_in ? 
               <div className="pl-0" styles="max-width: 15vw">{props.name}</div> :
               <Nav.Link href='/login' className="pl-0">Login / Signup</Nav.Link>
           }
-         
         </Navbar.Collapse>
     </Navbar>
     <br/>
